944cef7e05ef338941360c3b53197fff3ecfe0e6
AnesBenmerzoug <Anes.Benmerzoug@gmail.com>
2017-06-23 17:51:31 +0200

    Improved Mouse client

--- mouse/mouse_client.py
+++ mouse/mouse_client.py
@@ -82,10 +82,10 @@ class Mouse():
         print event
         if event.code == ecodes.REL_X:
             print "X Movement"
-            self.state[3] = int(event.value, 16)
+            self.state[3] = int(event.value, 2)
         elif event.code == ecodes.REL_Y:
             print "Y Movement"
-            self.state[4] = int(event.value, 16)
+            self.state[4] = int(event.value, 2)
 
 
     # poll for mouse events
