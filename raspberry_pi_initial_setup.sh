b88abf1ab9ff7f297a5fc00ecb42b20107de6c2a
AnesBenmerzoug <Anes.Benmerzoug@gmail.com>
2017-06-27 11:16:05 +0200

    Made some changes

--- raspberry_pi_initial_setup.sh
+++ raspberry_pi_initial_setup.sh
@@ -2,13 +2,20 @@
 
 # Update and upgrade raspberry pi
 
+echo "Updating and upgrading the Raspberry Pi"
+
 sudo apt-get update -y
 sudo apt-get updgrade -y
 sudo apt-get dist-upgrade -y
 
 # Install necessary packages
 
+echo "Installing the necessary packages"
+
 sudo apt-get install python-gobject pi-bluetooth bluez bluez-tools bluez-firmware python-bluez python-dev python-pip -y
+
+echo "Installing evdev for reading and writing input events from the keyboard and mouse"
+
 sudo pip install evdev -y
 
 # DBUS Configuration
