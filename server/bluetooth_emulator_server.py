8f2f198931104dc99299a982aa72cf42a8719cff
AnesBenmerzoug <Anes.Benmerzoug@gmail.com>
2017-06-28 23:10:40 +0200

    Made some changes

--- server/bluetooth_emulator_server.py
+++ server/bluetooth_emulator_server.py
@@ -69,7 +69,7 @@ class BluetoothDevice():
     P_CTRL = 17  # Service port - must match port configured in SDP record
     P_INTR = 19  # Service port - must match port configured in SDP record #Interrrupt port
     PROFILE_DBUS_PATH = "/bluez/upwork/hidbluetooth_profile"  # dbus path of  the bluez profile we will create
-    SDP_RECORD_PATH = sys.path[0] + "/sdp_record.xml"  # file path of the sdp record to laod
+    SDP_RECORD_PATH = sys.path[0] + "/sdp_record.xml"  # file path of the sdp record to load
     UUID = "00001124-0000-1000-8000-00805f9b34fb"
 
     def __init__(self):
