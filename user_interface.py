35a02ea0442d973f0860b579b07fc7ab79e761d9
AnesBenmerzoug <Anes.Benmerzoug@gmail.com>
2017-07-16 16:36:37 +0200

    Improved the user interface

--- user_interface.py
+++ user_interface.py
@@ -1,4 +1,6 @@
 from Tkinter import *
+import os
+import sys
 
 #####################################################################################################
 
@@ -10,21 +12,28 @@ class App(Frame):
         Frame.__init__(self, master, width=width, height=height, bg=background)
         self.pack(side="top", fill=BOTH, expand=True)
 
-        self.buttons_frame = Frame(self, bg=background)
+        self.buttons_frame = Frame(self, bg="grey")
         self.buttons_frame.pack(side="top", fill=X, expand=False)
 
         self.buttons_variable = IntVar()
         self.buttons_variable.set(0)
 
+        self.radio_buttons = []
+
         for i in xrange(4):
-            Radiobutton(self.buttons_frame, variable=self.buttons_variable, value=i, indicatoron=0).pack(fill=X, expand=True, side=LEFT, padx=10, pady=10)
+            self.radio_buttons.append(Radiobutton(self.buttons_frame, variable=self.buttons_variable, activebackground="green", bg="white",
+                                                  selectcolor="green", relief="sunken", command=self.change_screen, value=i, indicatoron=0))
+            self.radio_buttons[i].pack(fill=X, expand=True, side=LEFT, padx=10, pady=10)
+
+        self.change_screen()
 
         self.inner_frame = Frame(self, bg=background)
         self.inner_frame.pack(side="top", fill=X, expand=True)
-        self.pageOne = PageOne(self.inner_frame)
+        self.pageOne = PageOne(self.inner_frame).pack(side="top")
 
-        self.mouse = Mouse()
-        self.keyboard = Keyboard()
+    def change_screen(self):
+        index = self.buttons_variable.get()
+        print index
 
 class PageOne(Frame):
 
@@ -39,16 +48,17 @@ class PageOne(Frame):
         self.frame1 = Frame(self, bg=background)
         self.frame1.pack(fill=X)
 
-        Label(self.frame1, text="Bluetooth Status", bg=background).pack(side=LEFT, padx=(10,20), pady=10)
-        Label(self.frame1, textvariable=self.bluetooth_status, bg=background).pack(fill=X, expand=True, side=LEFT, padx=10, pady=10)
+        Label(self.frame1, text="Bluetooth Status: ", bg=background).pack(side=LEFT, padx=(10,20), pady=10)
+        Label(self.frame1, textvariable=self.bluetooth_status, bg="red").pack(fill=X, expand=True, side=LEFT, padx=10, pady=10)
 
         self.frame2 = Frame(self, bg=background)
         self.frame2.pack(fill=X)
 
-        Label(self.frame2, text="Connection Status", bg=background).pack(side=LEFT, padx=10, pady=10)
-        Label(self.frame2, textvariable=self.connection_status, bg=background).pack(fill=X, expand=True, side=LEFT, padx=10, pady=10)
+        Label(self.frame2, text="Connection Status: ", bg=background).pack(side=LEFT, padx=10, pady=10)
+        Label(self.frame2, textvariable=self.connection_status, bg="red").pack(fill=X, expand=True, side=LEFT, padx=10, pady=10)
 
 if __name__ == "__main__":
     root = Tk()
     root.minsize(300, 400)
+    root.maxsize(300, 400)
     App(root).mainloop()
