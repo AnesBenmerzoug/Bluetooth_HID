7b3d5817361ec16b8baf26edbe38fa7a9be359a3
AnesBenmerzoug <Anes.Benmerzoug@gmail.com>
2017-07-16 15:41:52 +0200

    Added some things to the bluetooth configuration script

--- bluetooth_daemon_configuration.sh
+++ bluetooth_daemon_configuration.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
 
-cmd1="sudo ifconfig wlan0 down"
+cmd1="sudo service dbus restart"
 
-cmd2="sudo /etc/init.d/bluetooth stop"
+cmd2="sudo rfkill block bluetooth"
 
-cmd3="sudo /usr/sbin/bluetoothd --nodetach --debug -p time"
+cmd3="sudo service bluetooth stop"
 
-cmd4="sudo hciconfig hci0 up"
+cmd4="sudo ifconfig wlan0 down"
+
+cmd5="sudo /etc/init.d/bluetooth stop"
+
+cmd6="sudo /usr/sbin/bluetoothd --nodetach --debug -p time"
+
+cmd7="sudo hciconfig hci0 up"
 
 ###########################################################################
 
@@ -16,12 +22,20 @@ sleep 1
 
 $cmd2 &>/dev/null &
 
+sleep 1
+
 $cmd3 &>/dev/null &
 
-sleep 1
-
 $cmd4 &>/dev/null &
 
 sleep 1
 
-$cmd4 &>/dev/null &
+$cmd5 &>/dev/null &
+
+sleep 1
+
+$cmd6 &>/dev/null &
+
+sleep 1
+
+$cmd7 &>/dev/null &
