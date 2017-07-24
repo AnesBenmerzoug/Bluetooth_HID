4b9d1f0c724d38dd44a8792fd8463409c8b85d11
AnesBenmerzoug <Anes.Benmerzoug@gmail.com>
2017-07-24 16:54:09 +0200

    Small changes

--- bluetooth_daemon_configuration.sh
+++ bluetooth_daemon_configuration.sh
@@ -2,13 +2,9 @@
 
 cmd1="sudo service dbus restart"
 
-cmd2="sudo rfkill block bluetooth"
-
 cmd3="sudo service bluetooth stop"
 
 cmd4="sudo /etc/init.d/bluetooth stop"
-
-cmd5="sudo rfkill unblock bluetooth"
 
 cmd6="sudo /usr/sbin/bluetoothd --nodetach --debug -p time"
 
@@ -22,19 +18,11 @@ $cmd1 &>/dev/null &
 
 sleep 0.2
 
-#$cmd2 &>/dev/null &
-
-sleep 0.2
-
 $cmd3 &>/dev/null &
 
 sleep 0.2
 
 $cmd4 &>/dev/null &
-
-sleep 0.2
-
-#$cmd5 &>/dev/null &
 
 sleep 0.2
 
